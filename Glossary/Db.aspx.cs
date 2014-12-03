39c483cf030f976d8f2d5da4ab75ed8e017cc500
mjpournassari <mjpournassari@gmail.com>
2014-12-03 21:35:04 +0330

    DB Added

---
+++ Glossary/Db.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Glossary
+{
+    public partial class Db : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string q = "";
+            System.IO.StreamReader sr = new System.IO.StreamReader(HttpContext.Current.Server.MapPath("1.sql"));
+
+            q = sr.ReadToEnd();
+
+
+            var sqlCommand = new SqlCommand();
+            sqlCommand.CommandText = q;
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.Connection = new SqlConnection("Data Source=.;Initial Catalog=glossary;Persist Security Info=True;User ID=dbuser;Password=Glossary123@");
+
+
+            sqlCommand.Connection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlCommand.Connection.Close();
+            sqlCommand.Dispose();
+        }
+    }
+}
