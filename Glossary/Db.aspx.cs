0addb2d5d96bd40b02c27fc916f83ab52b47fa0b
mjpournassari <mjpournassari@gmail.com>
2014-12-09 21:12:35 +0330

    DB file Edited

--- Glossary/Db.aspx.cs
+++ Glossary/Db.aspx.cs
@@ -13,22 +13,22 @@ namespace Glossary
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string q = "";
-            System.IO.StreamReader sr = new System.IO.StreamReader(HttpContext.Current.Server.MapPath("1.sql"));
+            //string q = "";
+            //System.IO.StreamReader sr = new System.IO.StreamReader(HttpContext.Current.Server.MapPath("1.sql"));
 
-            q = sr.ReadToEnd();
-
-
-            var sqlCommand = new SqlCommand();
-            sqlCommand.CommandText = q;
-            sqlCommand.CommandType = CommandType.Text;
-            sqlCommand.Connection = new SqlConnection("Data Source=.;Initial Catalog=glossary;Persist Security Info=True;User ID=dbuser;Password=Glossary123@");
+            //q = sr.ReadToEnd();
 
 
-            sqlCommand.Connection.Open();
-            sqlCommand.ExecuteNonQuery();
-            sqlCommand.Connection.Close();
-            sqlCommand.Dispose();
+            //var sqlCommand = new SqlCommand();
+            //sqlCommand.CommandText = q;
+            //sqlCommand.CommandType = CommandType.Text;
+            //sqlCommand.Connection = new SqlConnection("Data Source=.;Initial Catalog=glossary;Persist Security Info=True;User ID=dbuser;Password=Glossary123@");
+
+
+            //sqlCommand.Connection.Open();
+            //sqlCommand.ExecuteNonQuery();
+            //sqlCommand.Connection.Close();
+            //sqlCommand.Dispose();
         }
     }
 }
